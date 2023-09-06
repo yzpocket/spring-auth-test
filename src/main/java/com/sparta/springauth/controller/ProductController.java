bf63a1e9dbda29a81d6bd1bdaa8b72b6925144fa
INYONG KIM <ohnyong87@gmail.com>
2023-09-07 01:03:44 +0900

    feat : Test Spring Security Login

--- src/main/java/com/sparta/springauth/controller/ProductController.java
+++ src/main/java/com/sparta/springauth/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.sparta.springauth.controller;
 
 import com.sparta.springauth.entity.User;
+import com.sparta.springauth.security.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductController {
 
     @GetMapping("/products")
-    public String getProducts(HttpServletRequest req) {
-        System.out.println("ProductController.getProducts : 인증 완료");
-        User user = (User) req.getAttribute("user");
+    public String getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        // Authentcation 의 Principle
+        User user = userDetails.getUser();
         System.out.println("user.getUsername() = " + user.getUsername());
 
         return "redirect:/";
