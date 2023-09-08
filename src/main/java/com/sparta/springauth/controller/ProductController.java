5a1e33db84cb8bc3d60f73fbfd39e2cbf40b8266
INYONG KIM <ohnyong87@gmail.com>
2023-09-08 13:16:06 +0900

    feat : Add page accessibility management based on user role
    * Add @Secured Annotation on Controller request method
    * Add @EnableGlobalMethodSecurity(securedEnabled = true) on WebSecurityConfig to enable
    - Add forbidden special page for exceptional user

--- src/main/java/com/sparta/springauth/controller/ProductController.java
+++ src/main/java/com/sparta/springauth/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.sparta.springauth.controller;
 
 import com.sparta.springauth.entity.User;
+import com.sparta.springauth.entity.UserRoleEnum;
 import com.sparta.springauth.security.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,16 @@ public class ProductController {
         // Authentcation 의 Principle
         User user = userDetails.getUser();
         System.out.println("user.getUsername() = " + user.getUsername());
+
+        return "redirect:/";
+    }
+    @Secured(UserRoleEnum.Authority.ADMIN) // <- 관리자 전용 접근가능 페이지 설정방법
+    @GetMapping("/products/secured") // http://localhost:8080/api/products/secured -> 관리자 (ROLE이 ADMIN아니면 접속이 불가능하다.)
+    public String getProductsByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
+        for (GrantedAuthority authority : userDetails.getAuthorities()) {
+            System.out.println("authority.getAuthority() = " + authority.getAuthority());
+        }
 
         return "redirect:/";
     }
